e09ab3e53ebbd06c5240e350818d3aaf39ed83b6
Luke Strama <strama@ualberta.ca>
2018-08-27 15:53:39 -0600

    react-to-do-components-checkpoint

---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+    render() {
+        return (
+            <li> This will be a ToDo </li>
+        );
+    }
+}
+
+export default ToDo;
